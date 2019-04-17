03d029fd881ea689ca26c3d58836305d129ca44b
Anderson Caye <andersoncaye@gmail.com>
2019-04-17 19:54:39 -0300

    criação inicial

---
+++ logincsharp/logincsharp/App_Code/LoginBLL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+/// <summary>
+/// Descrição resumida de LoginBLL
+/// </summary>
+public class LoginBLL
+{
+
+    private string user { get; set; }
+    private string password { get; set; }
+
+    private DAL dal = new DAL();
+
+    public LoginBLL()
+    {
+    }
+
+    public DataTable RetListaUser()
+    {
+        return dal.RetDataTable("select * from login");
+    }
+
+    public void InsertUser()
+    {
+        string sql = "insert into login (user, pass) values ('{0}','{1}')";
+        sql = String.Format(sql, user, password);
+        dal.ExecutarComandoSQL(sql);
+    }
+}
