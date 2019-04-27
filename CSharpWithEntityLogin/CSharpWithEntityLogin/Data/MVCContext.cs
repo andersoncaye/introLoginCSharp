7b8cce84311a8c8f78f62cc7e4aae886e31dfb7d
Anderson Caye <andersoncaye@gmail.com>
2019-04-26 22:08:27 -0300

    nova arquitetura

---
+++ CSharpWithEntityLogin/CSharpWithEntityLogin/Data/MVCContext.cs
@@ -0,0 +1,24 @@
+﻿using CSharpWithEntityLogin.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.Entity;
+
+namespace CSharpWithEntityLogin.Data
+{
+    public class MVCContext : DbContext
+    {
+        
+        public MVCContext() 
+            : base("MVCContext_Desenv")
+        {
+
+        }
+
+        public DbSet<Usuario> Usuario { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ CSharpWithEntityLogin/CSharpWithEntityLogin/Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CSharpWithEntityLogin.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int Codigo { get; set; }
+        public string User { get; set; }
+        public string Senha { get; set; }
+    }
+}
